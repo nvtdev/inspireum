6a58068e392ef9cbd09b59a79fa0d42890ee94d9
nvtdev <nikolayivanov91@gmail.com>
2018-03-05 20:12:04 +0200

    small fix for heroku

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,26 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { MainService } from "./services/main.service";
 import { StoryService } from "./services/story.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
   appUser: Object;
 
   constructor(
     private mainService: MainService,
     private storyService: StoryService
-  ) { }
-  
-  ngOnInit() {
-    
-    if (localStorage.user) 
-      this.mainService.setUser(JSON.parse(localStorage.user));
+  ) {}
 
-    this.storyService.fetchAllStories(this.mainService.user['username']);
+  ngOnInit() {
+    if (localStorage.user)
+      this.mainService.setUser(JSON.parse(localStorage.user)),
+        this.storyService.fetchAllStories(this.mainService.user["username"]);
   }
 }
